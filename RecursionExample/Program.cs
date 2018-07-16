635bf62b7beb681508977b33e4a7999feff07467
DESKTOP-4RDBF36\tuana <tuananh300496@gmail.com>
2018-07-16 09:14:23 +0700

    Recursion Example

---
+++ RecursionExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RecursionExample
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+           Console.WriteLine(Factorial(5));
+            Console.WriteLine(Fibonacci(5));
+        }
+
+        private static int Factorial(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+            else
+            {
+                return n*Factorial(n - 1);
+            }
+        }
+
+        private static int Fibonacci(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+    }
+}
