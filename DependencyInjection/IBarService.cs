207082b3d34f6d221fe9d7fb834b0967221460ba
DESKTOP-4RDBF36\tuana <tuananh300496@gmail.com>
2018-07-25 13:57:35 +0700

    Example Dependency Injection With dotnet core 2.1

---
+++ DependencyInjection/IBarService.cs
@@ -0,0 +1,7 @@
+﻿namespace DependencyInjection
+{
+    public interface IBarService
+    {
+        void DoSomeRealWork();
+    }
+}
---
+++ DependencyInjection/IFooService.cs
@@ -0,0 +1,7 @@
+﻿namespace DependencyInjection
+{
+    public interface IFooService
+    {
+        void DoThing(int number);
+    }
+}
