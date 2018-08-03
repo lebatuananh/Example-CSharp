80c8bb33101131db9a2dfd895408e5529fc40838
DESKTOP-4RDBF36\tuana <tuananh300496@gmail.com>
2018-08-03 12:46:38 +0700

    Example Page Algotithm

---
+++ PagedAlgorithm/PagedResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace PagedAlgorithm
+{
+    public class PagedResult<T>:PagedResultBase where T:class
+    {
+        public PagedResult()
+        {
+            Results=new List<T>();
+        }
+
+        public IList<T> Results { get; set; }
+    }
+    
+}
---
+++ PagedAlgorithm/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PagedAlgorithm
+{
+    public class Student
+    {
+        public string Name { get; set; }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<Student>();
+            for (var i = 1; i <= 100; i++)
+            {
+                list.Add(new Student() {Name = "NameStudent " + i});
+            }
+            var paginationSet=new PagedResult<Student>()
+            {
+                Results = list,
+                CurrentPage = 2,
+                RowCount = list.Count,
+                PageSize = 8
+            };
+            Console.WriteLine(paginationSet.FirstRowOnPage);
+            Console.WriteLine(paginationSet.LastRowOnPage);
+            Console.WriteLine(paginationSet.PageCount);
+            Console.WriteLine(paginationSet.RowCount);            
+        }
+    }
+}
